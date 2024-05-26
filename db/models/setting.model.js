234ea7a63b3f95a0c481163e148d2f2e1e69af99
alaahaija <haija.alaa2018@gmail.com>
2024-05-26 14:52:38 +0300

    update setting

--- db/models/setting.model.js
+++ db/models/setting.model.js
@@ -2,7 +2,7 @@ import { Schema, model, Types } from "mongoose";
 
 const settingSchema = new Schema({
     phone:{
-        type:Number,
+        type:String,
         required:true,
     },
     email:{
